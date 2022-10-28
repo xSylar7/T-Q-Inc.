e6ee19ffee3f19d87ecceb281fda8497f804fd2a
Talal-12 <talal.alothman@gmail.com>
2022-10-28 09:02:48 +0300

    cleaned up Base & Home Templates + added Event model & registered in Admin

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,19 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+
+    name = models.CharField(max_length=50)
+    image = models.ImageField()
+    organizer = models.CharField(max_length=30)
+    seats = models.PositiveIntegerField(default=0)
+    price = models.CharField(max_length=30, blank=True, default="")
+    seats_booked = models.PositiveIntegerField(default=0)
+    date_event = models.DateField()
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
